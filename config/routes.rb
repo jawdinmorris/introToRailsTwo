88d55f0361ce18d6be25f67f6ab3e189ba98a26b
[jawdinmorris] <[jordanandrewmorris1@gmail.com]>
2018-03-26 13:50:41 +1000

    Can now view all notes attached to a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :notes
+  resources :users do
+   resources :notes #-> domain.com/users/:user_id/articles/
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
